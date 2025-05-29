8e8a3f5211052357ae8231047ce3a71994b4d516
dSeguraSanValero <a27238@svalero.com>
2025-05-29 23:02:31 +0200

    adasdaff

--- FrontEnd/js/editTreatment.js
+++ FrontEnd/js/editTreatment.js
@@ -14,6 +14,30 @@ window.onload = async function() {
 
     document.getElementById("treatmentCause").value = treatment.treatmentCause || "";
     document.getElementById("treatmentDate").value = treatment.treatmentDate || "";
+
+
+    const response = await fetch(muscularAssessmentUrl, {
+        method: "GET",
+        headers: {
+            "Authorization": `Bearer ${token}`,
+            "Content-Type": "application/json"
+        }
+    });
+
+    if (!response.ok) {
+        console.error("Error al obtener datos musculares:", response.status);
+        alert("No se pudieron obtener datos musculares.");
+        return;
+    }
+    
+    const muscularAssessment = await response.json();
+
+    document.getElementById("painLevel").value = muscularAssessment.painLevel || "";
+    document.getElementById("usualPhysicalActivity").value = muscularAssessment.usualPhysicalActivity || "";
+    document.getElementById("patientHeight").value = muscularAssessment.patientHeight || "";
+    document.getElementById("patientWeight").value = muscularAssessment.patientWeight || "";
+    document.getElementById("occupation").value = muscularAssessment.occupation || "";
+    document.getElementById("pastMedicalHistory").value = muscularAssessment.pastMedicalHistory || "";
 };
 
 $(document).ready(function(){
